cac752186747fd6e1d3cb114979bdc48d1668e5a
Ross Cadogan <rosscado@ie.ibm.com>
2018-07-13 14:14:04 +0100

    Add minimal unit tests

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -8,3 +8,13 @@ def client():
 	client = hello.app.test_client()
 
 	yield client
+
+def test_welcome(client):
+	"""Test application root includes a generic greeting"""
+	rv = client.get('/')
+	assert b'Welcome' in rv.data
+
+def test_get_visitors(client):
+	"""Test visitors api"""
+	rv = client.get('/api/visitors')
+	json_resp = rv.get_json()
