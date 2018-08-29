c5b7fb54be43c2daeda0a85fda381461d106eefd
Ross Cadogan <rosscado@ie.ibm.com>
2018-08-29 21:41:08 +0100

    Stages implementation

--- apps/api/logic.py
+++ apps/api/logic.py
@@ -79,15 +79,22 @@ a_club = api.model('Club', {
 	'url': fields.String(description="The club's primary web address", example='http://www.clontarfcycling.com/')
 	})
 
+a_stage = api.model('Stage', {
+	'distance_km': fields.Integer(description='The length of the stage in kilometres', example='80'),
+	'stage_type': fields.String(required=True, description='Is this stage a road race, time trial, etc?', enum=['road race', 'time trial', 'hill climb', 'criterium'], default='road race', example='road race'),
+	'start_time': fields.DateTime(description='What time does the stage start? ISO 8601 format: YYMM-MM-DD HH:MM', example='1970-01-01 13:00')
+	})
+
 an_event = api.model('Event', {
 	'id': fields.Integer(description='The unique identifier of an event (internal)', readonly=True),
 	'title': fields.String(required=True, description='The name of the event as promoted publically', example='The John Beggs Memorial'),
 	'date': fields.Date(required=True,description='When will the event take place? ISO 8601 format: YYYY-MM-DD', example='2018-08-11'),
 	'url': fields.String(description='The primary URL promoting the event', example='http://www.banbridgecc.com/thebeggs18/'),
 	'location': fields.String(description='The address of the event. Should identify at least the town.', example='Donore, Co. Down'),
-	'event_type': fields.String(required=True,description='The type of event. Is a road race, time trial, etc?', enum=['road race', 'time trial', 'hill climb', 'criterium', 'stage race'], default='road race', example='road race'),
+	'event_type': fields.String(required=True,description='Is the event a road race, time trial, etc?', enum=['road race', 'time trial', 'hill climb', 'criterium', 'stage race'], default='road race', example='road race'),
 	'status': fields.Nested(event_status, description='Records any schedule changes. If absent assume event is still scheduled normally'),
-	'organised_by': fields.Nested(a_club, description='Club hosting the event')
+	'organised_by': fields.Nested(a_club, description='Club hosting the event'),
+	'stages': fields.List(fields.Nested(a_stage, description='The stage list. One-day events have a single stage.'))
 	})
 
 data_store = TransientModel()
